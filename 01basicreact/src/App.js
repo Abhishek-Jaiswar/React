31e68d4bceda063fa5c8316bc2aabf47b3dd679c
Abhishek-Jaiswar <152155905+Abhishek-Jaiswar@users.noreply.github.com>
2023-11-27 15:32:39 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>heloo sir</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "chai or code"
+  return (
+    <>
+    <Chai/>
+    <h1>hello vite {username}</h1>
+    </>
+  )
+}
+
+export default App
